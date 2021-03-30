42d12f862c046a983c4e74aca4c54a8d2b50585e
jufei <jufei@baidu.com>
2021-03-30 10:05:29 +0800

    添加函数说明

--- 13 romanToInt.py
+++ 13 romanToInt.py
@@ -10,3 +10,23 @@
 Authors: jufei@baidu.com
 Date:    2021/3/30
 """
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        """
+        建立好映射表，依次做映射加减即可；
+        :param s:
+        :return:
+        """
+        ret = 0
+        char_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        for index, char in enumerate(s):
+            if index < len(s)-1 and char_int[char] < char_int[s[index+1]]:
+                ret -= char_int[char]
+            else:
+                ret += char_int[char]
+        return ret
--- 14 longestCommonPrefix.py
+++ 14 longestCommonPrefix.py
@@ -10,3 +10,35 @@
 Authors: jufei@baidu.com
 Date:    2021/3/30
 """
+
+
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        """
+        匹配各个字符串对应位置的字符是否相同，如果不同，则停止，注意字符串长度
+        :param strs:
+        :return:
+        """
+        if not strs:
+            return ''
+        prefix = ''
+        cur_status = True
+        cur_index = 0
+        while cur_status:
+            cur_char = None
+            for s in strs:
+                if cur_index >= len(s):
+                    cur_status = False
+                    break
+                if cur_char is None:
+                    cur_char = s[cur_index]
+                elif cur_char != s[cur_index]:
+                    cur_status = False
+                    break
+            if cur_status:
+                prefix += cur_char
+                cur_index += 1
+        return prefix
