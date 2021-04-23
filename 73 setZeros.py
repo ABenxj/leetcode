8272a7b4e2171a06703a8f4dfa0bc2e34350214a
居飞 <jufei@wecash.net>
2021-04-23 16:18:22 +0800

    面经修改

---
+++ 73 setZeros.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyhton
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 , Inc. All Rights Reserved
+#
+
+"""
+
+Authors: jufei
+Date: 2021/4/23 3:18 PM
+"""
---
+++ 75 sortColor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyhton
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 , Inc. All Rights Reserved
+#
+
+"""
+
+Authors: jufei
+Date: 2021/4/23 4:07 PM
+"""
